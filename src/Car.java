13053eb9e9eafa0714307b8687bfcb4177b1e1cd
boaralex2000 <boaralex10@gmail.com>
2019-01-30 12:14:51 +0100

    Car class
    Hela Uppgift A

---
+++ src/Car.java
@@ -0,0 +1,71 @@
+import javafx.scene.paint.Color;
+
+public class Car {
+
+	private final static double trimFactor = 1.25;
+
+	private double enginePower; // Engine power of the car
+	private double currentSpeed; // The current speed of the car
+	private Color color; // Color of the car
+	private String modelName; // The car model name
+
+	private boolean turboOn;
+	
+	public Car(Color c, String m, double ep) {
+		color = c;
+		modelName = m;
+		enginePower = ep;
+	}
+	
+	public double speedFactor() {
+		double turbo = 1;
+		if (turboOn)
+			turbo = 1.3;
+		return getEnginePower() * 0.01 * turbo;
+	}
+	
+	public double getEnginePower() {
+		return enginePower;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void startEngine() {
+		currentSpeed = 0.1;
+	}
+
+	public void stopEngine() {
+		currentSpeed = 0;
+	}
+
+	public void setTurboOn() {
+		turboOn = true;
+	}
+
+	public void setTurboOff() {
+		turboOn = false;
+	}
+
+	public void incrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+	}
+
+	public void decrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+	}
+
+	public void gas(double amount) {
+		incrementSpeed(amount);
+	}
+
+	public void brake(double amount) {
+		decrementSpeed(amount);
+	}
+	
+}
