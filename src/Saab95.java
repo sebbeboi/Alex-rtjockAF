94504fc535704363bd669807a6938f0ec2421d82
boaralex2000 <boaralex10@gmail.com>
2019-01-30 12:19:58 +0100

    Car classen
    Hela Uppgift A - Uppdaterad

--- src/Saab95.java
+++ src/Saab95.java
@@ -1,19 +1,10 @@
 import javafx.scene.paint.Color;
 
 public class Saab95 extends Car {
-
-	private boolean turboOn;
 	
 	private Saab95() {
 		super(Color.BLACK,"Saab95",125);
 		stopEngine();
-	}
-	
-	public double speedFactor() {
-		double turbo = 1;
-		if (turboOn)
-			turbo = 1.3;
-		return getEnginePower() * 0.01 * turbo;
 	}
 
 }
